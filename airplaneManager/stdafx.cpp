1eef28d97e6260acf10ca31cbcd4c3ae26a95184
alhimik45 <alhimik45@gmail.com>
2015-06-15 13:07:19 +0600

    renaming

--- airplaneManager/stdafx.cpp
+++ airplaneManager/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// airplanedb.pch will be the pre-compiled header
+// airplaneManager.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
